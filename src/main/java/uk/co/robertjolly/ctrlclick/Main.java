474b778836edd9f606f9fb38d2917cf21d188f29
raj24 <raj24@aber.ac.uk>
2018-07-27 04:52:16 +0100

    added gitignore and ctrlcraft main class

---
+++ src/main/java/uk/co/robertjolly/ctrlclick/Main.java
@@ -0,0 +1,9 @@
+package uk.co.robertjolly.ctrlclick;
+
+import net.minecraftforge.fml.common.Mod;
+import scala.tools.nsc.backend.icode.TypeKinds;
+import uk.co.robertjolly.ctrlclick.util.Reference;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+}
---
+++ src/main/java/uk/co/robertjolly/ctrlclick/util/Reference.java
@@ -0,0 +1,12 @@
+package uk.co.robertjolly.ctrlclick.util;
+
+public class Reference {
+
+    public static final String MOD_ID = "cm";
+    public static final String NAME = "Ctrl Craft";
+    public static final String VERSION = "0.1";
+    public static final String ACCEPTED_VERSIONS = "(1.12.2)";
+    public static final String CLIENT_PROXY_CLASS = "";
+    public static final String COMMON_PROXY_CLASS = "";
+
+}
